b11d1cce71ea5c85e615b19af6f9675ba83dd8c3
Ryan Birmingham <birm@rbirm.us>
2017-01-11 17:47:34 -0500

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,7 +1,16 @@
+import unittest
 from flask import Flask
 from flask_testing import TestCase
 from main import *
 
 class MyTest(TestCase):
     def create_app(self):
+        main.app.run(debug=True)
         return main.app
+
+def test_app_basic():
+    a = MyTest()
+    a.create_app()
+
+if __name__ == '__main__':
+    unittest.main()
